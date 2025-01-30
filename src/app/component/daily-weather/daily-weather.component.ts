27ba3d7a3bafb4352eb9a10f43e8f11d59113d86
Ryan <ryansabon@gmail.com>
2025-01-29 20:02:46 -0500

    Created the Daily Component in Angular

---
+++ src/app/component/daily-weather/daily-weather.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-weather',
+  standalone: true,
+  imports: [],
+  templateUrl: './daily-weather.component.html',
+  styleUrl: './daily-weather.component.css'
+})
+export class DailyWeatherComponent {
+
+}
