d18b4f36602b6b250a2b7f4e316ddc8805aac998
Ryan <ryansabon@gmail.com>
2025-01-30 20:16:30 -0500

    Added properties to Day, Location and Timeline interfaces

--- src/app/Interface/day.ts
+++ src/app/Interface/day.ts
@@ -1,2 +1,16 @@
+import { DailyWeatherComponent } from "../component/daily-weather/daily-weather.component";
+
 export interface Day {
+    dateTime:       string;     // Date and Time
+    tempMax:        number;     // High Temps
+    tempMin:        number;     // Low Temps
+    feelsLike:      number;     // Feels Like temperature
+    windSpeed:      number;     // Wind Speed
+    windDir:        string;     // Wind Direction
+    sunrise:        string;     // Sunrise time
+    sunset:         string;     // Sunset time
+    humidity:       number;     // Humidity 
+    description:    string;     // Weather Conditions
+    precipProb:     number;     // Precipitation %
+    hours:          Hour[];     // List of hours in the day        // Will need to import 'Hour' from Hour Component
 }
--- src/app/Interface/location.ts
+++ src/app/Interface/location.ts
@@ -1,2 +1,9 @@
+import { User } from "./user";
+
 export interface Location {
+    locationId:     number;     // Location ID for specified location
+    city:           string;     // Requested City
+    state:          string;     // Requested state
+    users:           User[];    // Represents the ICollection<User> from Backend
+    
 }
