d2e02c1c6ec5574c5b6a45e4e17b8326f87febe4
Ryan <ryansabon@gmail.com>
2025-02-03 11:46:09 -0500

    feat(daily-weather): add DailyWeather component with dummy weather data
    Added daily-weather.component.ts which defines the Angular component, initializes a dummy weather data object, and sets the component selector as <app-daily-weather>.
    
    Created daily-weather.component.html to render today's weather information (including icon, condition, temperature, and additional details).
    
    Developed daily-weather.component.css for basic styling of the component layout and elements.
    
    This commit sets the groundwork for displaying daily weather data and can be later updated to integrate with a live weather API.

--- src/app/component/daily-weather/daily-weather.component.ts
+++ src/app/component/daily-weather/daily-weather.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-daily-weather',
-  standalone: true,
-  imports: [],
-  templateUrl: './daily-weather.component.html',
-  styleUrl: './daily-weather.component.css'
+  selector:     'app-daily-weather',
+  templateUrl:  './daily-weather.component.html',
+  styleUrl:   './daily-weather.component.css'
 })
-export class DailyWeatherComponent {
+
+export class DailyWeatherComponent implements OnInit {
+  weather: any;
+  location: string = "Detroit, MI"
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.weather = {
+      dateTime:     new Date(),    // Date and Time
+      tempMax:      36,            // High Temps
+      tempMin:      22,            // Low Temps
+      feelsLike:    24,            // Feels Like temperature
+      windSpeed:    10,            // Wind Speed
+      windDir:      187,           // Wind Direction
+      sunrise:      "07:10 AM",    // Sunrise time
+      sunset:       "06:48 PM",    // Sunset time
+      humidity:     50,            // Humidity 
+      description:  "Cloudy",      // Weather Conditions
+      precipProb:   14,            // Precipitation %
+    };
+    
+  }
 
 }
