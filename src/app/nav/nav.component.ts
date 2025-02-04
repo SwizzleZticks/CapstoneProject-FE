1ff446402a1a8ab1a94d387d24dffbe5125a10a2
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2025-02-04 18:47:12 -0500

    added dropdown with styles, wired up login and logout feature

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {AccountService} from '../services/account.service';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  private accountService = inject(AccountService);
+  loggedIn = false;
+  isDropdownOpen = false;
   model: any = {};
 
   login(){
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+        this.isDropdownOpen = true;
+      },
+      error: error => {
+        console.log(error);
+      }
+    });
+  }
+
+  logout() {
+    this.loggedIn = false;
+    this.isDropdownOpen = false;
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
   }
 }
