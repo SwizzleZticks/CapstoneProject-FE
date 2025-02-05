552b6989537d127cfcaa182a048f5d781afcf7c9
Ryan <ryansabon@gmail.com>
2025-02-05 18:50:16 -0500

    weatherService created

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Day } from '../interfaces/day';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  private baseUrl = 'http://localhost:4200/'; 
+    
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getDailyWeather(location: string): Observable<Day> {
+    return this.http.get<Day>(`${this.baseUrl}forecast.json?q=${location}&days=1`);
+  }
 }
