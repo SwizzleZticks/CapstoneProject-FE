de4eddc4c2c699419d0f8e046adb2b422fb22b0a
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2025-02-06 18:30:42 -0500

    added missing imports

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class WeatherService {
 
   private http = inject(HttpClient);
-  baseUrl = 'https://localhost:7074/api/weather/'; 
+  baseUrl = 'https://localhost:7074/api/weather/';
 
   getDailyWeather() {
     return this.http.get(this.baseUrl + 'daily');
