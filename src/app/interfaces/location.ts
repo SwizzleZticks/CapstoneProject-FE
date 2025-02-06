13596ab390cdaabf8243a6d8096a215efd89fad4
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2025-02-05 20:28:46 -0500

    removed id property

--- src/app/interfaces/location.ts
+++ src/app/interfaces/location.ts
@@ -1,7 +1,6 @@
 import { User } from "./user";
 
 export interface Location {
-    locationId:     number;     // Location ID for specified location
     city:           string;     // Requested City
     state:          string;     // Requested state
 }
