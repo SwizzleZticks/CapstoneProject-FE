6742236dcdd675aa08ecee850aaa8125032b32c9
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2025-02-05 20:28:22 -0500

    added functionality for getting user info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NavComponent} from './nav/nav.component';
+import { NavComponent } from './nav/nav.component';
 import { DailyWeatherComponent } from "./component/daily-weather/daily-weather.component";
+import { HttpClient } from '@angular/common/http';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,31 @@ import { DailyWeatherComponent } from "./component/daily-weather/daily-weather.c
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Capstone-FE';
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  private accountService = inject(AccountService);
+  title = 'Capstone Weather';
+  users: any;
+
+  ngOnInit() {
+    this.getUsers();
+    this.setCurrentUser()
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem("user");
+    if (!userString) {
+      return;
+    }
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getUsers(){
+    this.http.get('http://localhost:5001/api/User').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
 }
